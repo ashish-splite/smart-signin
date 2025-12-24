6c42cee1d32482fd53ae647eb1601cc75dd4c65d
Ashish <ashish@Ashishs-MacBook-Pro.local>
2025-12-24 12:10:21 +0530

    added-cdk-and-infra-file

---
+++ infra/lib/infra-stack.ts
@@ -0,0 +1,33 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as iam from 'aws-cdk-lib/aws-iam';
+
+export class InfraStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // 1. Create the S3 Bucket
+    const bucket = new s3.Bucket(this, 'FrontendBucket', {
+      // Use the account and region dynamically
+      bucketName: `signin-frontend-bucket-${this.account}-${this.region}`,
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'index.html',
+      
+      // Public access settings
+      publicReadAccess: true, 
+      blockPublicAccess: new s3.BlockPublicAccess({
+        blockPublicAcls: false,
+        blockPublicPolicy: false,
+        ignorePublicAcls: false,
+        restrictPublicBuckets: false,
+      }),
+      
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // DeletionPolicy: Delete
+      autoDeleteObjects: true, // This fixes your "Bucket not empty" error!
+    });
+
+    // Note: CDK automatically handles the Bucket Policy when you set 
+    // publicReadAccess: true, so you don't need to write the IAM JSON manually!
+  }
+}
